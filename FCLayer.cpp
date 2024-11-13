314022cc8233255c8f1e773fdcdef881efd57f9d
lttinCS <162774530+tinletrongCS@users.noreply.github.com>
2024-11-13 23:18:25 +0700

    FCLayer.cpp

--- FCLayer.cpp
+++ FCLayer.cpp
@@ -1,10 +1,15 @@
-﻿/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt
- * to change this license Click
- * nbfs://nbhost/SystemFileSystem/Templates/cppFiles/class.cc to edit this
- * template
- */
 
+// ASSIGNMENT 2 ---------------------- *************** ----------------------
+/*
+* Faculity of Computer Science and Engineering
+* Author: Le Trong Tin
+* ID: 2313452
+* Date: November 8th 2024
+*/
+/*
+ * File:   DLinkedList.h
+ */
+// FILE ĐỊNH DẠNG MỚI
 /*
  * File:   FCLayer.cpp
  * Author: ltsach
@@ -15,7 +20,7 @@
 #include "layer/FCLayer.h"
 
 #include <exception>
-#include <filesystem>  //require C++17
+#include <filesystem> //require C++17
 #include <sstream>
 
 #include "ann/functions.h"
@@ -23,7 +28,8 @@
 namespace fs = std::filesystem;
 using namespace std;
 
-FCLayer::FCLayer(int Nin, int Nout, bool use_bias) {
+FCLayer::FCLayer(int Nin, int Nout, bool use_bias)
+{
   this->m_nNin = Nin;
   this->m_nNout = Nout;
   this->m_bUse_Bias = use_bias;
@@ -34,7 +40,8 @@ FCLayer::FCLayer(int Nin, int Nout, bool use_bias) {
 }
 
 FCLayer::FCLayer(string sParams, string filename_w, string filename_b,
-                 string sName) {
+                 string sName)
+{
   // update name
   if (trim(sName).size() != 0)
     this->m_sName = sName;
@@ -42,22 +49,28 @@ FCLayer::FCLayer(string sParams, string filename_w, string filename_b,
     m_sName = "FC_" + to_string(++m_unLayer_idx);
 
   // parse sParams to get Nin, Nout, use-bias
-  try {
+  try
+  {
     // extract Nin, Nout, use-bias from sParams:
     char delimiter = ',';
     istringstream param_stream(sParams);
-    int nparams[] = {0, 0, 0};            // for: Nin, Nout, use_bias
-    string param_slist[] = {"", "", ""};  // for: Nin, Nout, use_bias
+    int nparams[] = {0, 0, 0};           // for: Nin, Nout, use_bias
+    string param_slist[] = {"", "", ""}; // for: Nin, Nout, use_bias
     int idx = 0;
-    while (getline(param_stream, param_slist[idx], delimiter)) {
+    while (getline(param_stream, param_slist[idx], delimiter))
+    {
       nparams[idx] = stoi(param_slist[idx]);
       idx++;
-      if (idx >= 3) break;  // just use the first three elements
+      if (idx >= 3)
+        break; // just use the first three elements
     }
-    if (idx == 2) {
+    if (idx == 2)
+    {
       cout << "use-bias: not specified; to use-bias=true" << endl;
       nparams[2] = 1;
-    } else if (idx < 2) {
+    }
+    else if (idx < 2)
+    {
       throw std::runtime_error(
           "FC's parameters: must specify at least Nin and Nout");
     }
@@ -69,7 +82,8 @@ FCLayer::FCLayer(string sParams, string filename_w, string filename_b,
 
     bool weight_file_invalid = !fs::exists(filename_w);
     bool bias_file_invalid = m_bUse_Bias && !fs::exists(filename_b);
-    if (weight_file_invalid) {
+    if (weight_file_invalid)
+    {
       // DO INITIALIZATION when files are invalid
       // Weight file is not specified correctly => initialize randomly
       string message = fmt::format(
@@ -80,21 +94,25 @@ FCLayer::FCLayer(string sParams, string filename_w, string filename_b,
       // initialize
       this->m_aWeights = xt::random::randn<double>({m_nNout, m_nNin});
       this->m_aGrad_W = xt::zeros<double>({m_nNout, m_nNin});
-    } else {
+    }
+    else
+    {
       // DO LOADING WEIGHTS when the file are valid
       double_tensor W = xt::load_npy<double>(filename_w);
       bool valid = (W.dimension() == 2) && (W.shape()[0] == m_nNout) &&
                    (W.shape()[1] == m_nNin);
-      if (!valid) {
+      if (!valid)
+      {
         throw std::runtime_error(
             "FC::Weights: shape from data file is not the same with the "
             "specification");
       }
       this->m_aWeights = W;
       this->m_aGrad_W =
-          xt::zeros<double>({m_nNout, m_nNin});  // initialize gradW
+          xt::zeros<double>({m_nNout, m_nNin}); // initialize gradW
     }
-    if (bias_file_invalid) {
+    if (bias_file_invalid)
+    {
       // Bias file is not specified correctly => initialize with 0
       string message = fmt::format(
           "{:s}: not exist; so initialize biases with 0", filename_b);
@@ -103,9 +121,12 @@ FCLayer::FCLayer(string sParams, string filename_w, string filename_b,
       // initialize
       this->m_aBias = xt::zeros<double>({m_nNout});
       this->m_aGrad_b = xt::zeros<double>({m_nNout});
-    } else {
+    }
+    else
+    {
       // DO LOADING BIAS when the file are valid
-      if (m_bUse_Bias) {
+      if (m_bUse_Bias)
+      {
         double_tensor b = xt::load_npy<double>(filename_b);
         bool valid = (b.dimension() == 1) && (b.shape()[0] == m_nNout);
         if (!valid)
@@ -115,10 +136,12 @@ FCLayer::FCLayer(string sParams, string filename_w, string filename_b,
 
         // loading
         this->m_aBias = b;
-        this->m_aGrad_b = xt::zeros<double>({m_nNout});  // initialize gradW
+        this->m_aGrad_b = xt::zeros<double>({m_nNout}); // initialize gradW
       }
     }
-  } catch (exception& e) {
+  }
+  catch (exception &e)
+  {
     string message = fmt::format(string("\tsParams: \"{:s}\";\r\n") +
                                      string("\tfilename_w: \"{:s}\";\r\n") +
                                      string("\tfilename_b: \"{:s}\";\r\n") +
@@ -129,22 +152,25 @@ FCLayer::FCLayer(string sParams, string filename_w, string filename_b,
     cerr << "-------EXCEPTION: BEGIN-------" << endl;
     cerr << "\t" << e.what() << endl;
     cerr << "-------EXCEPTION: END-------" << endl;
-    throw;  // re-throw to the caller
+    throw; // re-throw to the caller
   }
 }
 
-void FCLayer::init_weights() {
+void FCLayer::init_weights()
+{
   this->m_aWeights = xt::random::randn<double>({m_nNout, m_nNin});
   this->m_aGrad_W = xt::zeros<double>({m_nNout, m_nNin});
 
-  if (m_bUse_Bias) {
+  if (m_bUse_Bias)
+  {
     // this->m_aBias = xt::random::randn<double>({m_nNout});
     this->m_aBias = xt::zeros<double>({m_nNout});
     this->m_aGrad_b = xt::zeros<double>({m_nNout});
   }
 }
 
-FCLayer::FCLayer(const FCLayer& orig) {
+FCLayer::FCLayer(const FCLayer &orig)
+{
   m_sName = "FC_" + to_string(++m_unLayer_idx);
 }
 
@@ -153,39 +179,36 @@ FCLayer::~FCLayer() {}
 // LAN TRUYỀN THUẬN
 xt::xarray<double> FCLayer::forward(xt::xarray<double> X)
 {
-    if (this->has_learnable_param())
-    {
-        this->m_aCached_X = X;
-    }
-    xt::xarray<double> result = xt::linalg::tensordot(X, this->m_aWeights, { 1 }, { 0 });
-    if (this->m_bUse_Bias)
-    {
-        result += this->m_aBias;
-    }
-    result += 0;
-
-    return result;
+  if (m_trainable)
+  {
+    m_aCached_X = X;
+  }
+  double_tensor Y;
+  Y = xt::linalg::dot(X, xt::transpose(m_aWeights));
+  if (this->m_bUse_Bias)
+  {
+    Y += m_aBias;
+  }
+  return Y;
 }
 
-// LAN TRUYỀN NGƯỢC 
+// LAN TRUYỀN NGƯỢC
 xt::xarray<double> FCLayer::backward(xt::xarray<double> DY)
 {
-    this->m_unSample_Counter = DY.shape()[0];
-    this->m_aGrad_W = xt::linalg::tensordot(DY, m_aCached_X, { 0 }, { 0 });
-    // tính delta b nếu có sử dụng bias
-    if (this->m_bUse_Bias)
-    {
-        this->m_aGrad_b = xt::sum(DY, { 0 });
-    }
-
-    xt::xarray<double> DX = xt::linalg::tensordot(DY, this->m_aWeights);
-    return DX;
+  m_aGrad_b = xt::sum(DY, {0});
+  double_tensor dW_batch = xt::zeros<double>({m_aWeights.shape()[0], m_aWeights.shape()[1]});
+  dW_batch = outer_stack(DY, m_aCached_X);
+  m_aGrad_W = xt::sum(dW_batch, {0});
+  double_tensor DX = xt::linalg::dot(DY, m_aWeights);
+  return DX;
 }
 
-int FCLayer::register_params(IParamGroup* ptr_group) {
+int FCLayer::register_params(IParamGroup *ptr_group)
+{
   ptr_group->register_param("weights", &m_aWeights, &m_aGrad_W);
   int count = 1;
-  if (m_bUse_Bias) {
+  if (m_bUse_Bias)
+  {
     ptr_group->register_param("bias", &m_aBias, &m_aGrad_b);
     count += 1;
   }
@@ -193,18 +216,21 @@ int FCLayer::register_params(IParamGroup* ptr_group) {
   return count;
 }
 
-string FCLayer::get_desc() {
+string FCLayer::get_desc()
+{
   string desc = fmt::format("{:<10s}, {:<15s}: {:<4d}, {:<4d}, {:<4d}", "FC",
                             this->getname(), this->m_nNin, this->m_nNout,
                             this->m_bUse_Bias);
   return desc;
 }
-void FCLayer::save(string model_path) {
+void FCLayer::save(string model_path)
+{
   string filename_w = model_path + "/" + this->getname() + "_W.npy";
   string filename_b = model_path + "/" + this->getname() + "_b.npy";
 
   xt::dump_npy(filename_w, m_aWeights);
-  if (m_bUse_Bias) {
+  if (m_bUse_Bias)
+  {
     xt::dump_npy(filename_b, m_aBias);
   }
 }
@@ -225,44 +251,58 @@ void FCLayer::save(string model_path) {
  *  + bias-file does not exist: use-bias=false;
  *
  */
-void FCLayer::load(string model_path, string layer_name) {
+void FCLayer::load(string model_path, string layer_name)
+{
   layer_name = trim(layer_name);
 
   string filename_w, filename_b;
-  if (layer_name.size() == 0) {
+  if (layer_name.size() == 0)
+  {
     filename_w = model_path + "/" + this->getname() + "_W.npy";
     filename_b = model_path + "/" + this->getname() + "_b.npy";
-  } else {
+  }
+  else
+  {
     filename_w = model_path + "/" + layer_name + "_W.npy";
     filename_b = model_path + "/" + layer_name + "_b.npy";
   }
 
-  try {
-    if (fs::exists(filename_w)) {
+  try
+  {
+    if (fs::exists(filename_w))
+    {
       // DO LOADING from the file
       m_aWeights = xt::load_npy<double>(filename_w);
       m_nNin = m_aWeights.shape()[1];
       m_nNout = m_aWeights.shape()[0];
       m_aGrad_W = xt::zeros<double>({m_nNout, m_nNin});
-    } else {
+    }
+    else
+    {
       string message =
           fmt::format("{:s}: weight-file does not exist.", filename_w);
       throw std::runtime_error(message);
     }
-    if (fs::exists(filename_b)) {
+    if (fs::exists(filename_b))
+    {
       m_aBias = xt::load_npy<double>(filename_b);
-      if (m_aBias.shape()[0] != m_nNout) {
+      if (m_aBias.shape()[0] != m_nNout)
+      {
         throw "Number of values in m_aBias must be the same as Nout.";
       }
       m_aGrad_b = xt::zeros<double>({m_nNout});
       m_bUse_Bias = true;
-    } else {
+    }
+    else
+    {
       m_bUse_Bias = false;
     }
 
     m_unSample_Counter = 0;
-  } catch (exception& e) {
+  }
+  catch (exception &e)
+  {
     cout << e.what() << endl;
-    throw;  // re-throw to the caller
+    throw; // re-throw to the caller
   }
 }
