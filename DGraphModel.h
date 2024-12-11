edd28af4a71f1dc27ee00e32477d3b4764336e1d
lttinCS <162774530+tinletrongCS@users.noreply.github.com>
2024-12-12 01:18:52 +0700

    DGraphModel.h

--- DGraphModel.h
+++ DGraphModel.h
@@ -1,20 +1,16 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/*
  * File:   DGraphModel.h
  * Author: LTSACH
  *
  * Created on 23 August 2020, 19:36
  */
-
+/**
+ * Người thực hiện : LÊ TRỌNG TÍN
+ * MSSV : 2313452
+ */
 #ifndef DGRAPHMODEL_H
 #define DGRAPHMODEL_H
 #include "graph/AbstractGraph.h"
-#include "AbstractGraph.h"
 //////////////////////////////////////////////////////////////////////
 ///////////// GraphModel: Directed Graph Model    ////////////////////
 //////////////////////////////////////////////////////////////////////
@@ -57,7 +53,7 @@ public:
         {
             throw VertexNotFoundException(AbstractGraph<T>::vertex2str(to));
         }
-        
+
         typename AbstractGraph<T>::VertexNode *fromNode = this->getVertexNode(from);
         typename AbstractGraph<T>::VertexNode *toNode = this->getVertexNode(to);
 
@@ -76,20 +72,26 @@ public:
     void remove(T vertex)
     {
         // Xóa một đỉnh và tất cả các cạnh liên quan đến đỉnh đó
-        typename AbstractGraph<T>::VertexNode *nodeToDelete= this->getVertexNode(vertex);
+        typename AbstractGraph<T>::VertexNode *nodeToDelete = this->getVertexNode(vertex);
 
         if (nodeToDelete == nullptr)
         {
             throw VertexNotFoundException(AbstractGraph<T>::vertex2str(vertex));
         }
-
         for (auto it : this->nodeList)
         {
-            typename AbstractGraph<T>::VertexNode* pVertex = it;
-            nodeToDelete->removeTo(pVertex);
-            pVertex->removeTo(nodeToDelete);
+            typename AbstractGraph<T>::VertexNode *pVertex = it;
+            if (this->connected(vertex, pVertex->getVertex()))
+            {
+                nodeToDelete->removeTo(pVertex);
+            }
+
+            if (this->connected(pVertex->getVertex(), vertex))
+            {
+                pVertex->removeTo(nodeToDelete);
+            }
         }
-        
+
         this->nodeList.removeItem(nodeToDelete);
 
         delete nodeToDelete;
@@ -97,11 +99,11 @@ public:
 
     static DGraphModel<T> *create(T *vertices, int nvertices, Edge<T> *edges, int nedges, bool (*vertexEQ)(T &, T &), string (*vertex2str)(T &))
     {
-        // *vertices là mảng các vertices
         DGraphModel<T> *new_graph = new DGraphModel<T>(vertexEQ, vertex2str);
         for (int i = 0; i < nvertices; i++)
         {
-            new_graph->add(vertices[i]);
+            typename AbstractGraph<T>::VertexNode* node =  new typename AbstractGraph<T>::VertexNode(vertices[i], vertexEQ, vertex2str);
+            new_graph->nodeList.add(node);
         }
         for (int i = 0; i < nedges; i++)
         {
